0f88ac33ef6939c8c0f3ca981cebb68263dc14a1
Justin <justin.tseng@kingwaytek.com>
2022-04-19 17:19:57 +0800

    14 create a new iOS project without storyboards

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by justin on 2022/4/19.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .orange
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
+
