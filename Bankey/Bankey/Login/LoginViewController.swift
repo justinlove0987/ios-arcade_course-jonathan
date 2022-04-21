dd997f3263c7c1dd05748dfee7ca3b527ffef2fa
Justin <justin.tseng@kingwaytek.com>
2022-04-21 16:47:53 +0800

    15 add LoginView into LoginViewController

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -10,12 +10,31 @@ import UIKit
 class LoginViewController: UIViewController {
     
     // MARK: - Properties
+    
+    private let loginView = LoginView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
     }
 
 
+}
+
+extension LoginViewController {
+    private func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+    }
 }
 
