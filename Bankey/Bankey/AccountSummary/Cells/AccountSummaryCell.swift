376a63bead95ec86ad56117976f48b6f0d4af9cc
Justin <justin.tseng@kingwaytek.com>
2022-05-13 14:49:15 +0800

    66 Adding the table view cell

---
+++ Bankey/Bankey/AccountSummary/Cells/AccountSummaryCell.swift
@@ -0,0 +1,39 @@
+//
+//  AccountSummaryCell.swift
+//  Bankey
+//
+//  Created by justin on 2022/5/13.
+//
+
+import Foundation
+import UIKit
+
+class AccountSummaryCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setup()
+        layout()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
+extension AccountSummaryCell {
+    
+    private func setup() {
+        
+    }
+    
+    private func layout() {
+        
+    }
+    
+}
+
